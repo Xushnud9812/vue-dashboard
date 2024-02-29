bebef8bdb58100ca8210c1944920f7ffd1732a63
Umidbek <ujumaniyozov@ya.ru>
2024-02-29 15:56:06 +0500

    ASD

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -35,7 +35,7 @@ const sidebarMenu = ref([
 <template>
   <div>
     <div :class="sidebarStore.isSidebar ? 'left-0' : '-left-full '"
-      class="fixed duration-300 p-5 top-0  w-72 h-screen z-50 bg-blue-600">
+      class="fixed duration-300 p-5 top-0  w-72 h-screen z-50 bg-[#166199]">
       <div>
         <img class="mx-auto" src="/logo.png" alt="">
       </div>
@@ -43,7 +43,7 @@ const sidebarMenu = ref([
 
       <div class="flex flex-col mt-10 gap-2">
         <router-link
-          class="[&.router-link-exact-active]:bg-[#1d4ed8] flex items-center gap-2 text-white p-3 rounded-md hover:bg-[#1d4ed8]"
+          class="[&.router-link-exact-active]:bg-[#114E7B] flex items-center gap-2 text-white p-3 rounded-md hover:bg-[#114E7B]"
           v-for="item, index in sidebarMenu" :key="index" :to="item.url">
           <Icon :icon="`${item.icon}`" width="28" height="28"/>
           <p class="text-bold">{{ item.name }}</p>
