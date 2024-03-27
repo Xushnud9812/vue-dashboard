18c3ac2adb604deab0ce880023377cc6ad8b4927
Umidbek <ujumaniyozov@ya.ru>
2024-03-27 13:47:16 +0500

    SD

--- src/pages/manager/group/create-group.vue
+++ src/pages/manager/group/create-group.vue
@@ -1,3 +1,41 @@
+<script setup>
+import { useRouter } from 'vue-router'
+import { api } from '@/api'
+import { ref } from 'vue'
+
+const router = useRouter()
+const getData = ref([])
+const close = () => {
+  router.go(-1)
+}
+const fetchData = async () => {
+  try {
+    const response = await api.get(`group/get-data`);
+    getData.value = response.data
+    console.log('rr', getData.value)
+  } catch (e) {
+    console.log('e', e)
+  }
+}
+const formData = ref({
+  direction_id: null,
+  day: null,
+  room_id: null,
+  time: null,
+  start_date: null
+})
+const createGroup = async () => {
+  try {
+    await api.post('/group/create', formData._value)
+    window.location.reload();
+  } catch (e) {
+
+  }
+}
+fetchData()
+
+
+</script>
 <template>
   <div class="">
     <div @click="close" class="fixed top-0 left-0 w-full h-screen bg-black/70 z-50"></div>
@@ -11,43 +49,32 @@
       </div>
 
       <div class="flex flex-col gap-5">
-        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" name="" id="">
-          <option value="1">1</option>
-          <option value="1">2</option>
+        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" v-model="formData.direction_id">
+          <option v-for="item, index in getData.direction" :key="index" :value="item.id">{{ item.name }}</option>
         </select>
-        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" name="" id="">
-          <option value="1">1</option>
-          <option value="1">2</option>
+        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" v-model="formData.day">
+          <option v-for="item, index in getData.day" :key="index" :value="item.id">{{ item.name }}</option>
         </select>
-        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" name="" id="">
-          <option value="1">1</option>
-          <option value="1">2</option>
+        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" v-model="formData.time">
+          <option v-for="item, index in getData.time" :key="index" :value="item.id">{{ item.name }}</option>
         </select>
-        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" name="" id="">
-          <option value="1">1</option>
-          <option value="1">2</option>
+        <select class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" v-model="formData.room_id">
+          <option v-for="item, index in getData.room" :key="index" :value="item.id">{{ item.name }}</option>
         </select>
       </div>
 
       <div class="mt-10">
         <p class="text-gray-400">Kurs boshlanish sanasi</p>
-        <input class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" type="date">
+        <input class="w-full focus:outline-none pr-12 bg-gray-100 px-4 py-2 rounded" type="date"
+          v-model="formData.start_date">
       </div>
-
-
+      <button class="bg-[#166199] rounded py-2.5 px-5 flex gap-1 text-white mt-4" @click="createGroup">
+        <Icon class="text-lg" icon="ep:plus" />
+        Qo'shish
+      </button>
     </div>
   </div>
 </template>
 
-<script setup>
-import { useRouter } from 'vue-router'
-
-const router = useRouter()
-
-const close = () => {
-  router.go(-1)
-}
-
-</script>
 
 <style lang="scss" scoped></style>
