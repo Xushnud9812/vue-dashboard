d1117ee93f15bae29a74bff4ad6aafece300672b
Umidbek <ujumaniyozov@ya.ru>
2024-03-29 21:34:57 +0500

    ASD

--- src/pages/call-center/leads.vue
+++ src/pages/call-center/leads.vue
@@ -1,4 +1,4 @@
-<script setup >
+<script setup>
 import { ref } from "vue";
 import LeadCard from "@/components/LeadCard.vue";
 import { useKanbanStore } from "@/store/lead";
@@ -23,7 +23,8 @@ const createTask = (title) => {
   <div class="flex gap-x-5">
     <div class="w-1/4 rounded overflow-hidden" v-for="column in kanbanStore.columns">
       <div class="bg-white  p-5 flex items-center justify-between">
-        <h1>{{ column.title }} <span class="bg-primary px-2 text-white ml-2 rounded-full">{{ column.tasks.length }}</span>
+        <h1>{{ column.title }} <span class="bg-primary px-2 text-white ml-2 rounded-full">{{ column.tasks.length
+            }}</span>
         </h1>
         <button v-if="column.title == 'Yangi lidlar'" @click="createTask(column.title)"
           class="bg-primary text-white py-1  rounded px-2">
